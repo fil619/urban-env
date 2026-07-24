0803cbc0bf7e1bb8c2a0dee8b1e5cb9b4c9662aa
fil619 <filbertcrasto619@gmail.com>
2026-07-24 22:47:28 +0100

    Install Vue Router

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@
 // Composables
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { router } from "./router";
 
 // Plugins
 import { registerPlugins } from "@/plugins";
@@ -25,4 +26,5 @@ const app = createApp(App);
 registerPlugins(app);
 
 app.use(pinia);
+app.use(router);
 app.mount("#app");
